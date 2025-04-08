1a1bca7df6975fdb7fee64fb3373fb1fd97eb693
hanna <nsmp.dev@gmail.com>
2025-04-08 16:29:16 -0500

    planning queen

--- role.queen.js
+++ role.queen.js
@@ -13,6 +13,12 @@ Creep.prototype.runQueen = function () {
         if (this.store[RESOURCE_ENERGY] == 0) {
             // grab the storage in the room
             let target = this.room.storage;
+
+            // if the storage has any ingredients in it and this isn't a capitol
+                // assign a task to gather the ingredient from the storage
+            // if the terminal has any ingredients in it and this is a capitol
+                // assign a task to gather the ingredient from the terminal
+
             // if the storage isn't built or is empty
             if (target == undefined || target.store[RESOURCE_ENERGY] == 0) {
                 // assign a new task
@@ -26,6 +32,12 @@ Creep.prototype.runQueen = function () {
                 this.announceTask();
             }
         }else{
+
+            // if the creep has non-energy and the room is not a capitol
+                // assign a task to drop it off at the terminal
+            // if the creep has non-energy and the room is a capitol
+                // assign a task to drop it off at the storage
+
             // find a new dump target fit for a queen
             let target = this.getQueenDumpTarget();
             // if a new target was found
